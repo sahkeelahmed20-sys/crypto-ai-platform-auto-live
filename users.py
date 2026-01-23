cce3150ad06e5dfefdd563268b22b70e214d4796
sahkeelahmed20-sys <sahkeelahmed20@gmail.com>
2026-01-23 02:07:00 +0100

    Update users.py

--- users.py
+++ users.py
@@ -1,34 +1,10 @@
-from passlib.context import CryptContext
-from jose import jwt
-from datetime import datetime, timedelta
-from database import get_db
-from config import JWT_SECRET, JWT_ALGORITHM, JWT_EXPIRE_MINUTES
+from sqlalchemy import Column, Integer, String
+from database import Base
 
-pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
+class User(Base):
+    __tablename__ = "users"
 
-def register_user(username, password):
-    db = get_db()
-    hashed = pwd.hash(password)
-    db.execute(
-        "INSERT INTO users (username, password) VALUES (?,?)",
-        (username, hashed)
-    )
-    db.commit()
-
-def authenticate(username, password):
-    db = get_db()
-    user = db.execute(
-        "SELECT * FROM users WHERE username=?",
-        (username,)
-    ).fetchone()
-
-    if not user or not pwd.verify(password, user["password"]):
-        return None
-
-    payload = {
-        "id": user["id"],
-        "admin": bool(user["is_admin"]),
-        "exp": datetime.utcnow() + timedelta(minutes=JWT_EXPIRE_MINUTES)
-    }
-    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
-    return token
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+    role = Column(String, default="user")  # admin / user

c4d2923238878c89dfba9de052b2e7f7bd3c9b94
sahkeelahmed20-sys <sahkeelahmed20@gmail.com>
2026-01-23 01:56:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 from fastapi import FastAPI
-from auth import router as auth_router
-
 from fastapi.middleware.cors import CORSMiddleware
+from auth import router as auth_router
 
 app = FastAPI(title="Crypto AI Platform")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # later you can restrict this
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# ✅ ROUTERS MUST COME AFTER app EXISTS
 app.include_router(auth_router)
-
 
 @app.get("/")
 def root():

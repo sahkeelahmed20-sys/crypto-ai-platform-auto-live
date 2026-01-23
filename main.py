6e243ed9f1038d5f15c62fae3a48519cd8ac6476
sahkeelahmed20-sys <sahkeelahmed20@gmail.com>
2026-01-23 21:37:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from database import Base, engine
 from auth import router as auth_router
 from stats import router as stats_router
 import time
+from market import router as market_router
+
 
 app = FastAPI(title="Crypto AI Platform")
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(market_router)
 
 app.include_router(stats_router)
 

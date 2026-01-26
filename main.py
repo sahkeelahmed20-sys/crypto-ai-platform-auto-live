3f77976fd69e0af7ff58185a10de8b18f4933290
sahkeelahmed20-sys <sahkeelahmed20@gmail.com>
2026-01-26 22:18:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from database import Base, engine
-from auth import router as auth_router
-from stats import router as stats_router
-import time
+
 from market import router as market_router
-from trades import router as trades_router
-from market_ws import router as ws_router
-
-
-
-
 
 app = FastAPI(title="Crypto AI Platform")
 
-Base.metadata.create_all(bind=engine)
-
-app.include_router(trades_router)
-
 app.include_router(market_router)
 
-app.include_router(ws_router)
-
-app.include_router(stats_router)
-
-app.include_router(auth_router)
-
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
-@app.get("/market/candles")
-def get_candles(symbol: str):
-    # TEMP: mock candles (we replace with real exchange next)
-    now = int(time.time())
-    candles = []
-
-    price = 43000
-    for i in range(60):
-        open_ = price
-        close = open_ + (-200 + i * 5)
-        high = max(open_, close) + 50
-        low = min(open_, close) - 50
-
-        candles.append({
-            "time": now - (60 - i) * 60,
-            "open": open_,
-            "high": high,
-            "low": low,
-            "close": close
-        })
-        price = close
-
-    return candles
-    
-@app.get("/stats/summary")
-def stats_summary():
-    return {
-        "total_trades": 0,
-        "win_rate": 0,
-        "pnl": 0
-    }
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def root():
